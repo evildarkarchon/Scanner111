486551c77a4db7f7ac7c78869bd57e7c268611f3
Andrew Nelson <evildarkarchon@gmail.com>
2025-04-16 03:22:12 -0700

    Update Avalonia packages to version 11.2.7
    Upgraded all Avalonia-related dependencies from version 11.2.5 to 11.2.7. This includes adjustments in `project.assets.json`, build properties, and associated package references. Ensures compatibility with the latest updates and fixes in Avalonia 11.2.7.

--- Scanner111/obj/Debug/net8.0/Scanner111.AssemblyInfo.cs
+++ Scanner111/obj/Debug/net8.0/Scanner111.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scanner111")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e622734cedc0d349169cf2b5a2f622900bf919b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+902c7a2941e733ecac77479887cea24d231b386c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scanner111")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scanner111")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
