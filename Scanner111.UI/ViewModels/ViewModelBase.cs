d58965653900fca6579d070bf67f679c2b63f321
Andrew Nelson <evildarkarchon@gmail.com>
2025-04-18 04:18:00 -0700

    Refactor to replace CommunityToolkit with ReactiveUI
    Switched ViewModelBase to inherit from ReactiveObject instead of ObservableObject and removed custom Avalonia data validation logic. This streamlines the implementation by fully adopting ReactiveUI and reducing redundant code.

--- Scanner111.UI/ViewModels/ViewModelBase.cs
+++ Scanner111.UI/ViewModels/ViewModelBase.cs
@@ -1,7 +1,7 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using ReactiveUI;
 
 namespace Scanner111.UI.ViewModels;
 
-public class ViewModelBase : ObservableObject
+public class ViewModelBase : ReactiveObject
 {
 }
