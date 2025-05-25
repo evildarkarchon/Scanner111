c1b61bbc061af39954b4a3400846cf1efb462817
Andrew Nelson <evildarkarchon@gmail.com>
2025-05-24 17:28:44 -0700

    Starting Over, again

--- Scanner111/App.axaml.cs
+++ Scanner111/App.axaml.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Linq;
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
-using Scanner111.Services;
+
 using Scanner111.ViewModels;
 using Scanner111.Views;
 
 namespace Scanner111;
 
-public class App : Application
+public partial class App : Application
 {
-    /// <summary>
-    ///     Gets or sets the application's service provider.
-    /// </summary>
-    public static IServiceProvider? ServiceProvider { get; set; }
-
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -26,33 +18,19 @@ public class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
-
-            // Create main window
-            var mainWindow = new MainWindow();
-
-            // Create dialog service
-            var dialogService = new DialogService(mainWindow);
-
-            // Create and set view model with dialog service
-            mainWindow.DataContext = new MainWindowViewModel(dialogService);
-
-            // Set as main window
-            desktop.MainWindow = mainWindow;
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = new MainViewModel()
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainView
+            {
+                DataContext = new MainViewModel()
+            };
         }
 
         base.OnFrameworkInitializationCompleted();
-    }
-
-    private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
-
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove) BindingPlugins.DataValidators.Remove(plugin);
     }
 }
